8bcb887e0dd13e82aa3be1e89bb4113051f778b9
JEON-Sungsu <anagma@naver.com>
2024-05-06 23:47:37 +0900

    feat: 홈 화면 API 연동, RefreshToken 을 활용한 토큰 재발급 로직 추가

--- lib/restaurant/presentation/restaurant.dart
+++ lib/restaurant/presentation/restaurant.dart
@@ -1,8 +1,24 @@
+import 'package:delivery_app/common/const/data.dart';
 import 'package:delivery_app/restaurant/component/restaurant_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Restaurant extends StatelessWidget {
   const Restaurant({super.key});
+
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {'authorization': 'Bearer $accessToken'},
+      ),
+    );
+
+    return resp.data['data'];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,14 +26,37 @@ class Restaurant extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            tags: ['떡볶이', '치즈', '매운맛'],
-            name: '불타는 떡볶이',
-            ratingCount: 100,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-            rating: 4.52,
+          child: FutureBuilder<List>(
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+              return ListView.separated(
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+
+                  return RestaurantCard(
+                    image: Image.network(
+                      'http://$ip${item['thumbUrl']}',
+                      fit: BoxFit.cover,
+                    ),
+                    tags: List<String>.from(item['tags']),
+                    name: item['name'],
+                    ratingsCount: item['ratingsCount'],
+                    deliveryTime: item['deliveryTime'],
+                    deliveryFee: item['deliveryFee'],
+                    ratings: item['ratings'],
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return const SizedBox(
+                    height: 16,
+                  );
+                },
+                itemCount: snapshot.data!.length,
+              );
+            },
           ),
         ),
       ),
